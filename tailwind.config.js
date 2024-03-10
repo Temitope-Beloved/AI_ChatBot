d5932ff6803bac7cb041770102818db6cbfc62f6
Temitope-Beloved <olafusisam236@gmail.com>
2024-03-10 13:35:50 -0700

    addins some files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,36 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+// tailwind.config.js
+const { nextui } = require("@nextui-org/react");
 
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    // ...
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+     
+      extend: {
+        colors: {
+          primary: "#00040f",
+          secondary: "#00f6ff",
+          dimWhite: "rgba(255, 255, 255, 0.7)",
+          dimBlue: "rgba(9, 151, 124, 0.1)",
+        },
+        fontFamily: {
+          poppins: ["Poppins", "sans-serif"],
+        },
+      },
+      screens: {
+        xs: "480px",
+        ss: "620px",
+        sm: "768px",
+        md: "1060px",
+        lg: "1200px",
+        xl: "1700px",
+      },
+  },
+  darkMode: "class",
+
+
+  plugins: [nextui()],
+};
